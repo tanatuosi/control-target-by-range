de38e37751e4cee5ee470b6be69f8473d6de9436
tanatuosi <49973816+tanatuosi@users.noreply.github.com>
2023-07-04 10:39:52 +0800

    完成功能

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 const range = document.getElementById("range") as HTMLInputElement;
 const box = document.querySelector(".box") as HTMLDivElement;
+
+range.addEventListener("input", function () {
+  const value = Number(this.value);
+  box.style.borderRadius = `${value / 2}px`;
+  const middle = (value / 100) * 127 + 128;
+  const left = (value / 100) * 255;
+  box.style.backgroundColor = `rgb(${left},${middle},0)`;
+  box.style.marginLeft = `${value * 3}px`;
+});
+
+range.value = "0";
